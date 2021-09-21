9e5da756fc5c92533e283299bfffc7889435055a
mrheat <mrheat777@gmail.com>
2021-09-21 12:29:14 +0200

    Changed  SegwitHeight als auch MinBIP9WarningHeight to current height + 2000 blocks.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -81,8 +81,8 @@ public:
         consensus.BIP66Height = 130000;
         /* Doichain activates CSV/Segwit with BIP16.  */
         consensus.CSVHeight = 475000;
-        consensus.SegwitHeight = 475000;
-        consensus.MinBIP9WarningHeight = 477016; // segwit activation height + miner confirmation window
+        consensus.SegwitHeight = 206000;
+        consensus.MinBIP9WarningHeight = 208000; // segwit activation height + miner confirmation window
         consensus.powLimit = uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
